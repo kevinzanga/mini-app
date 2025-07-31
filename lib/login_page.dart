f1558f8ab7098e0226ff0ace92dc6dab5945d5b0
kevinzanga <32467980+kevinzanga@users.noreply.github.com>
2025-07-30 20:33:36 -0400

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -75,7 +75,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: Column(
         children: [
